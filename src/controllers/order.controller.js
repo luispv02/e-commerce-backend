b815c2f8e0945778cfc08dad15f02191a92d07b9
Luis Fernando <luispv.1002@gmail.com>
2026-05-06 11:38:47 -0600

    feat(order): sort orders by creation date in getOrders function

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -3,7 +3,7 @@ const Order = require("../models/Order.model");
 const getOrders = async(req, res, next) => {
   try {
     const userId = req.user.uid;
-    const orders = await Order.find({ user: userId });
+    const orders = await Order.find({ user: userId }).sort({ createdAt: -1 });
 
     return res.status(200).json({
       ok: true,
